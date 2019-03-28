15c08e752fe330425215af6b85259a9a1b0255b4
GBTC440001UR <ajhenley@gmail.com>
2019-03-27 20:15:55 -0400

    Initial commit

---
+++ src/main/java/com/byaj/demo45678/Demo45678Application.java
@@ -0,0 +1,13 @@
+package com.byaj.demo45678;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo45678Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo45678Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/byaj/demo45678/Sue.java
@@ -0,0 +1,6 @@
+package com.byaj.demo45678;
+
+public class Sue {
+    public int bob = 5;
+    public int sam =12;
+}
